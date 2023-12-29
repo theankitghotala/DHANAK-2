92fa7ad81e3fde5a1cbe232f0801462a8886a058
Ankit mishra <143910171+theankitghotala@users.noreply.github.com>
2023-12-29 19:19:52 +0530

    Update1 script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import{DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader.js'
-console.log(DRACOLoader)
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 // import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 // import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
@@ -28,6 +27,7 @@ const sizes = {
 width: window.innerWidth,
 height: window.innerHeight,
 };
+
 //debug gui
 const gui = new dat.GUI()
 
@@ -45,10 +45,7 @@ window.addEventListener('resize',()=>
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio,10))
     renderer.setClearAlpha()
-
-
 })
-
 
 // For turning window in full screen
 window.addEventListener('dblclick', () => {
@@ -60,6 +57,7 @@ window.addEventListener('dblclick', () => {
         document.exitFullscreen()
     }
 })
+
 // *******FUNCTION FOR CONTROLLING BY MOUSE********
 window.addEventListener('mousemove', (event) => {
 cursor.x = event.clientX / sizes.width - 0.5;
@@ -73,27 +71,30 @@ cursor.y = -(event.clientY / sizes.height - 0.5);
  */
 const scene = new THREE.Scene();
 scene.fog = new THREE.Fog( 0x01AFA3, 3, 18 );
+
 /**
  * CAMERA
  */
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+
 /**
  * RENDERER
  */
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-// renderer.shadowMap.type = THREE.PCFSoftShadowMap  // shadow radius doen't work with this map 
-// renderer.shadowMap.enabled = true;
 renderer.setClearColor(0x01AFA3, 1);
 renderer.outputColorSpace = THREE.SRGBColorSpace
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap  // shadow radius doen't work with this map 
+// renderer.shadowMap.enabled = true;
 
 // Texture Loader
 const textureloader = new THREE.TextureLoader()
-const bakedTexture = textureloader.load('static/baked2.jpg')
+const bakedTexture = textureloader.load('static/baked2.jpg') // TEXTURE FOR BLENDER MODEL 
 bakedTexture.flipY = false
 bakedTexture.colorSpace = THREE.SRGBColorSpace 
+
 // Textures
 // const imagetexture = textureloader.load('Assets/background.jpg')
 // 	imagetexture.wrapS = THREE.RepeatWrapping;
@@ -102,20 +103,6 @@ bakedTexture.colorSpace = THREE.SRGBColorSpace
 // scene.background = imagetexture
 
 // const context = canvas.getContext('2d');
-
-// // Generate scratchy pattern using Perlin noise
-// for (let i = 0; i < canvas.width; i++) {
-//   for (let j = 0; j < canvas.height; j++) {
-//     const value = Math.floor(Math.random() * 505);
-//     context.fillStyle = `rgb(${value},${value},${value})`;
-//     context.fillRect(i, j, 1, 1);
-//   }
-// }
-// const texture = new THREE.CanvasTexture(canvas);
-// texture.wrapS = THREE.RepeatWrapping;
-// texture.wrapT = THREE.RepeatWrapping;
-// texture.repeat.set(1, 1); // Adjust the repeat values as needed
-// scene.background = imagetexture;
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff,0.3);
 directionalLight.position.set(0.2,1.06,0.19)
@@ -141,32 +128,23 @@ scene.add( directionalLight );
  */
 const material2 = new THREE.MeshStandardMaterial({
 	color: 0x01AFA3,
-	// map: imagetexture
 })
 material2.metalness = 0.2
 material2.roughness = 0
 
 const material3 = new THREE.MeshBasicMaterial({
-    // color: 0xff0000,
     map: bakedTexture
-}
-
-)
+})
 // gui.add(material2,'metalness').min(0).max(1).step(0.0001)
 // gui.add(material2,'roughness').min(0).max(1).step(0.0001)
 
 // 
 let loadedModel;
-// NEW WAY TO DO add geometry as well as material
 gltfLoader.load('static/FULL LOGO.glb',
     (gltf) =>{
         gltf.scene.traverse((child) =>{
             child.material = material3
         })
-        // gltf.scene.position.y = -1
-        // gltf.scene.position.x = 2
-        // // gltf.scene.rotation.y 
-        // scene.add(gltf.scene)
         // Store the loaded model for later manipulation
         loadedModel = gltf.scene;
         loadedModel.position.y = -1
@@ -176,18 +154,6 @@ gltfLoader.load('static/FULL LOGO.glb',
         scene.add(loadedModel);
     }
 )
-
-
-// const sphere = new THREE.Mesh(
-//     new THREE.SphereGeometry(1.5,50,100),
-//     material2
-// )
-// sphere.position.x = 3
-// sphere.rotation.x += 1
-// // sphere.position.z = -5
-// sphere.castShadow = true
-// scene.add(sphere)
-
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(10,10),
     material2
@@ -206,6 +172,9 @@ const controls = new OrbitControls( camera, renderer.domElement);
 controls.enableDamping = true;
 // controls.dampingFactor = 0; // Adjust damping factor as needed
 
+/**
+*Don't coomentout Because I have removed MATCAPS from aasets
+*/
 // const fontLoader = new FontLoader()
 // fontLoader.load(
 //     'static/textures/Fonts/optimer_bold.typeface.json', 
@@ -229,36 +198,29 @@ controls.enableDamping = true;
 //         textGeometry.center()
 //         // Textures 
 //         const textureloader = new THREE.TextureLoader()
-//         const matcapTexture = textureloader.load('static/textures/matcaps/matcap (7).png')
+//         const matcapTexture = textureloader.load('static/textures/matcaps/matcap (7).png') 
 //         const material = new THREE.MeshMatcapMaterial({
 //             // flatShading: true,
 //             matcap: matcapTexture
 //         })
 //         const text =  new THREE.Mesh(textGeometry,material)
 //         scene.add(text)
-       
-
-//         console.log('font loaded')
 //     }
 // )
 //clock
 const clock = new THREE.Clock()
-let oldElapsedTime = 0
+// let oldElapsedTime = 0
 //*****************ANIMATION************
 function animate() {
 	const elapsedTime = clock.getElapsedTime()
-	const deltaTime = elapsedTime-oldElapsedTime
-	oldElapsedTime = elapsedTime
-	// console.log(deltaTime)
+	// const deltaTime = elapsedTime-oldElapsedTime
+	// oldElapsedTime = elapsedTime
 	camera.lookAt(new THREE.Vector3())
 	
     // // // Rotate the model
     if (loadedModel) {
-        loadedModel.rotation.y = elapsedTime*0.1; // Adjust the rotation speed as needed
-        // loadedModel.rotation.y = elapsedTime*0.01; // Adjust the rotation speed as needed
+        loadedModel.rotation.y = elapsedTime*0.1;
     }
-
-
 
 	controls.update();
 	renderer.render( scene, camera );
